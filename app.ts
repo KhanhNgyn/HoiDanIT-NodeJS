8517d17d53def377f4ae0f591367896ee933d274
NguyenDoKhanh <khanhndhe182074@fpt.edu.vn>
2026-01-06 21:02:58 +0700

    video 25

--- app.ts
+++ app.ts
@@ -1,7 +1,12 @@
-const express = require("express");
+// const express = require("express");
+import express from "express"
 
 const app = express();
 const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello Word");
+})
 
 app.get("/hoidanit", (req, res) => {
     res.send("Hello Word Eric");
