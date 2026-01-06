11668505c745932fb69c1557ce2b00767f6e90da
NguyenDoKhanh <khanhndhe182074@fpt.edu.vn>
2026-01-06 20:48:28 +0700

    video 24

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello word")
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+app.get("/hoidanit", (req, res) => {
+    res.send("Hello Word Eric");
+})
+
+app.listen(8080, () => {
+    console.log(`My app is running on port: ${PORT}`)
+});
